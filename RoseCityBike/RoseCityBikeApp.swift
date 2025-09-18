12551c12df21465f54e64144a1e1d2cfc1b92b26
Oscar Epp <oepp@uoregon.edu>
2025-09-18 10:06:59 -0700

    Initial Commit

---
+++ RoseCityBike/RoseCityBikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoseCityBikeApp.swift
+//  RoseCityBike
+//
+//  Created by Oscar Epp on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct RoseCityBikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RoseCityBikeTests/RoseCityBikeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoseCityBikeTests.swift
+//  RoseCityBikeTests
+//
+//  Created by Oscar Epp on 9/18/25.
+//
+
+import Testing
+@testable import RoseCityBike
+
+struct RoseCityBikeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
